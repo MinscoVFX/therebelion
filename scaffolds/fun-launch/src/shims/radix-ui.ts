bb6598426c11c407f943b512a548b0000ca61380
MinscoVFX <bajum300@gmail.com>
2025-08-14 04:52:36 +0000

    chore: add radix shims, normalize prettier, install deps

---
+++ scaffolds/fun-launch/src/shims/radix-ui.ts
@@ -0,0 +1,7 @@
+export * from "@radix-ui/react-tabs";
+export * from "@radix-ui/react-dialog";
+export * from "@radix-ui/react-checkbox";
+export * from "@radix-ui/react-popover";
+export * from "@radix-ui/react-hover-card";
+export * from "@radix-ui/react-tooltip";
+export * from "@radix-ui/react-select";
