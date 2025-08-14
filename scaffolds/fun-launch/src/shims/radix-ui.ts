fad1da9013c8d6187ae10742db1f893b8c425dde
MinscoVFX <bajum300@gmail.com>
2025-08-14 05:05:34 +0000

    fix(scaffold): map @/* to src/*; remove duplicate Radix shim

--- scaffolds/fun-launch/src/shims/radix-ui.ts
+++
@@ -1,7 +0,0 @@
-export * from "@radix-ui/react-tabs";
-export * from "@radix-ui/react-dialog";
-export * from "@radix-ui/react-checkbox";
-export * from "@radix-ui/react-popover";
-export * from "@radix-ui/react-hover-card";
-export * from "@radix-ui/react-tooltip";
-export * from "@radix-ui/react-select";
