cc9ba4d80cbc5bb9f674bd1acd5a124e15d9fe50
MinscoVFX <bajum300@gmail.com>
2025-09-13 15:57:21 -0400

    Update exit.tsx

--- scaffolds/fun-launch/src/pages/exit.tsx
+++ scaffolds/fun-launch/src/pages/exit.tsx
@@ -1,7 +1,9 @@
-import Page from '@/components/ui/Page/Page';
-import OneClickExitAutoButton from '@/components/OneClickExitAutoButton';
+import React from 'react';
+// Use relative paths to avoid tsconfig path alias issues in CI
+import Page from '../../components/ui/Page/Page';
+import OneClickExitAutoButton from '../../components/OneClickExitAutoButton';
 
-export default function ExitPage() {
+export default function ExitPage(): JSX.Element {
   return (
     <Page>
       <div className="p-6 space-y-6">
